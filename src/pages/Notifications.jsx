4ca6523002d1d4ba14f56f82c8510bdf72522d04
Finaxis002 <finaxis.ai@gmail.com>
2025-04-18 17:44:31 +0530

    admin notify

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import EmployeeNotifications from "../Components/notification/EmployeeNotification";
 import AdminNotification from "../Components/notification/AdminNotification";
-
+import NotificationBell from "../Components/notification/NotificationBell";
 const Notifications = () => {
  
   return (
     <div className="text-white p-4">
       <EmployeeNotifications />
       <AdminNotification />
+      <NotificationBell />
     </div>
   );
 };
